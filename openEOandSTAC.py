d4a46c832a8db834a68a2560eb2ef28283dabb71
yuvraj1989 <yuvraj@gmail.com>
2025-01-14 14:42:10 +0100

    Complete pystac instance setup with catalog, collection and Items

--- openEOandSTAC.py
+++ openEOandSTAC.py
@@ -1,30 +1,43 @@
 from pystac import Catalog, Collection, Item
+import pystac
 import os
 import re
+import pystac.media_type
 import rasterio
 from shapely.geometry import Polygon, mapping
+from datetime import datetime
+from pystac.extensions.eo import Band, EOExtension
 
 # Create a catalog
-catalog =  Catalog(id='sentinal_data', description='Sentinal Data Catalog')
+catalog = Catalog(id='sentinal_data', description='Sentinal Data Catalog')
 
-# Create collection for Bolzano's sentinel-2 data
-collection = Collection(id='Sentinel-2-Bolzano', description='Sentinel-2 data for Bolzano',)
+# List of bands
+bands = [Band.create(name='B04', common_name='red', center_wavelength=0.665, full_width_half_max=0.025, description='Red 665 nm'),Band.create(name='B08', common_name='nir', center_wavelength=0.842, full_width_half_max=0.145, description='Near-Infrared 842 nm')]
 
-# Add spatial and temporal extent to the collection
-collection.extent = {
-    'spatial':[[11.3, 46.4], [11.5, 46.5]],
-    'temporal':[['2024-01-01', '2024-01-31']]
-}
-
-catalog.add_collection(collection)
+# List of collection item
+collection_items = []
 
 # Populate the collection with items
 def add_item_to_collection(image_folder):
     files = os.listdir(image_folder)
     for file in files:
         if file.endswith('.tif'):
-            print(image_folder+file)
-        item = Item()
+            bbox, footprint = get_bbox_and_footprint(image_folder + file)
+            item = Item(id='Sentinel-2-Bolzano-' + extract_date(file), geometry=footprint, bbox=bbox, datetime=datetime.utcnow(), properties={})
+            item.common_metadata.gsd = 10
+            item.common_metadata.platform = 'Sentinel-2'
+            item.common_metadata.instruments = ['MSI']
+            # create asset to add to the item
+            collection_asset = pystac.Asset(href=image_folder+file, media_type = pystac.MediaType.GEOTIFF)
+            item.add_asset('image', collection_asset)
+            # Apple EO extension to the asset
+            eo = EOExtension.ext(item.assets['image'], add_if_missing = True)
+            eo.apply(bands)
+            collection_items.append(item)
+
+def get_collection_interval(collection_item_list):
+    sorted_list = sorted(collection_item_list, key=lambda x: x.datetime)
+    return sorted_list
 
 # Extract date from the file name
 def extract_date(file_name):
@@ -32,10 +45,10 @@ def extract_date(file_name):
 
     if match:
         extracted_date = match.group(1)  # Get the matched date
-        print("Extracted Date:", extracted_date)
+        return extracted_date
     else:
-        print("No date found in the filename.")
-    return extracted_date
+        return None
+
 
 # Get the bounding box and footprint of the raster file
 def get_bbox_and_footprint(raster):
@@ -50,3 +63,17 @@ def get_bbox_and_footprint(raster):
         ])
         
         return (bbox, mapping(footprint))
+
+add_item_to_collection('./ndvi_images/')
+collection_interval = get_collection_interval(collection_items)
+
+# get tempral extent from collection items
+collection_interval = pystac.TemporalExtent(intervals=[collection_interval])
+# Collection extent
+collection_extent = pystac.Extent(spatial= [[11.3, 46.4], [11.5, 46.5]], temporal=[['2024-01-01', '2024-01-31']])
+
+# Create collection for Bolzano's sentinel-2 data
+collection = Collection(id='Sentinel-2-Bolzano', description='Sentinel-2 data for Bolzano', extent = collection_extent, license='CC-BY-SA-4.0')
+collection.add_items(collection_items)
+catalog.add_child(collection)
+catalog.describe()
